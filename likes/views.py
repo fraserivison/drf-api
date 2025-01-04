7ff65fbd1403b904f6eb6288add813328d81a4ff
fraserivison <fraserivison@hotmail.co.uk>
2025-01-04 10:28:39 +0000

    Add LikesDetail view

--- likes/views.py
+++ likes/views.py
@@ -1,14 +1,25 @@
 from rest_framework import generics, permissions
 from drf_api.permissions import IsOwnerOrReadOnly
-from .models import Like
-from .serializers import LikeSerializer
+from likes.models import Like
+from likes.serializers import LikeSerializer
+
 
 class LikeList(generics.ListCreateAPIView):
-    """ List all likes. Create a like if authenticated. The perform_create method associates the like with the logged in user. """
-
+    """
+    List likes or create a like if logged in.
+    """
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = LikeSerializer
     queryset = Like.objects.all()
-    
+
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a like or delete it by id if you own it.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
