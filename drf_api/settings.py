b6b55e00a1edde4ac2fcc6bba921fd4c9a9e4d3e
fraserivison <fraserivison@hotmail.co.uk>
2025-01-08 14:47:45 +0000

    Update CORS settings to try fix auth issue on frontend

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -58,7 +58,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = True #'DEV' in os.environ
 
-ALLOWED_HOSTS = ['*', 'wave-app-c1bcdf0519e2.herokuapp.com', 'wave-drf-api-1157a4fa181b.herokuapp.com']
+ALLOWED_HOSTS = [
+    '*',
+    'wave-app-c1bcdf0519e2.herokuapp.com',
+    'wave-drf-api-1157a4fa181b.herokuapp.com',
+]
 
 # CSRF and CORS settings
 CSRF_TRUSTED_ORIGINS = [
@@ -68,13 +72,19 @@ CSRF_TRUSTED_ORIGINS = [
     "https://wave-drf-api-1157a4fa181b.herokuapp.com",
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "https://wave-app-c1bcdf0519e2.herokuapp.com",
-    "https://3000-fraserivison-waveapp-xp3woia850v.ws-eu117.gitpod.io",
-    "https://8000-fraserivison-drfapi-d10c7zwdb71.ws-eu117.gitpod.io",
-    "https://wave-drf-api-1157a4fa181b.herokuapp.com",
-]
+#CORS_ALLOWED_ORIGINS = [
+   #"https://wave-app-c1bcdf0519e2.herokuapp.com",
+    #"https://3000-fraserivison-waveapp-xp3woia850v.ws-eu117.gitpod.io",
+    #"https://8000-fraserivison-drfapi-d10c7zwdb71.ws-eu117.gitpod.io",
+    #"https://wave-drf-api-1157a4fa181b.herokuapp.com",
+#]
 
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [os.environ.get('CLIENT_ORIGIN')]
+else:
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        r"^https://.*\.gitpod\.io$",
+    ]
 CORS_ALLOW_CREDENTIALS = True
 #CORS_ALLOW_ALL_ORIGINS = False
 
