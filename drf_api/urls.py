8d643d27a0f7725cfe84d617516c29a69d0ffedb
fraserivison <fraserivison@hotmail.co.uk>
2025-01-08 13:01:17 +0000

    Comment out url paths to fix 500 error in heroku

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -11,10 +11,8 @@ urlpatterns = [
     path('dj-rest-auth/logout/', logout_route),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-    path('auth/social/', include('allauth.socialaccount.urls')),
-    path('accounts/', include('allauth.urls')),
-    
-    # Include app-specific URLs
+    #path('auth/social/', include('allauth.socialaccount.urls')),
+    #path('accounts/', include('allauth.urls')),
     path('profiles/', include('profiles.urls')),
     path('tracks/', include('tracks.urls')),
     path('comments/', include('comments.urls')),
@@ -24,7 +22,7 @@ urlpatterns = [
 ]
 
 # Serve media files in development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG:
+    #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
