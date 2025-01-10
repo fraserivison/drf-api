73e621708a4c8b11d3acf8ec36e51bb08ec54d6a
fraserivison <fraserivison@hotmail.co.uk>
2025-01-10 16:31:21 +0000

    Fix RatingSerializer to display track title as well as ID by using the correct field title

--- ratings/serializers.py
+++ ratings/serializers.py
@@ -1,25 +1,28 @@
-from django.db import IntegrityError
 from rest_framework import serializers
 from ratings.models import Rating
 
 class RatingSerializer(serializers.ModelSerializer):
     """
-    Serializer for the Rating model
+    Serializer for the Rating model.
     The create method handles updating an existing rating if it already exists.
     """
     owner = serializers.ReadOnlyField(source='owner.username')
-    title_name = serializers.ReadOnlyField(source='title.name')  # Added title_name to show track title
+    title_name = serializers.SerializerMethodField()  # Use SerializerMethodField to fetch the track title
 
     class Meta:
         model = Rating
-        fields = ['id', 'created_at', 'owner', 'title', 'title_name', 'rating']  # Added title_name to the fields
+        fields = ['id', 'created_at', 'owner', 'title', 'title_name', 'rating']  # Add 'title_name' to the fields
+
+    def get_title_name(self, obj):
+        # Use the 'title' field from the related Track model
+        return obj.title.title if obj.title else None  # Adjusted to use the correct field name ('title')
 
     def create(self, validated_data):
         # Check if the user has already rated the track
         user = validated_data.get('owner')
-        title = validated_data.get('title')  # Updated to use 'title'
+        title = validated_data.get('title')  # Using 'title' instead of 'track'
 
-        existing_rating = Rating.objects.filter(owner=user, title=title).first()  # Updated to use 'title'
+        existing_rating = Rating.objects.filter(owner=user, title=title).first()
 
         if existing_rating:
             # If a rating already exists, update it instead of creating a new one
@@ -28,4 +31,8 @@ class RatingSerializer(serializers.ModelSerializer):
             return existing_rating
         else:
             return super().create(validated_data)
+
+
+
+
 
