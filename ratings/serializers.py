1f9f843b7c54005234eed89cf68756b0d9b934ed
fraserivison <fraserivison@hotmail.co.uk>
2025-01-10 15:08:03 +0000

    Allow users to update their previous ratings for tracks by removing the unique constraint on ratings and adjusting the serializer to handle rating overrides

--- ratings/serializers.py
+++ ratings/serializers.py
@@ -5,7 +5,7 @@ from ratings.models import Rating
 class RatingSerializer(serializers.ModelSerializer):
     """
     Serializer for the Rating model
-    The create method handles the unique constraint on 'owner' and 'track'
+    The create method handles updating an existing rating if it already exists.
     """
     owner = serializers.ReadOnlyField(source='owner.username')
 
@@ -14,9 +14,17 @@ class RatingSerializer(serializers.ModelSerializer):
         fields = ['id', 'created_at', 'owner', 'track', 'rating']
 
     def create(self, validated_data):
-        try:
+        # Check if the user has already rated the track
+        user = validated_data.get('owner')
+        track = validated_data.get('track')
+
+        existing_rating = Rating.objects.filter(owner=user, track=track).first()
+
+        if existing_rating:
+            # If a rating already exists, update it instead of creating a new one
+            existing_rating.rating = validated_data['rating']
+            existing_rating.save()
+            return existing_rating
+        else:
             return super().create(validated_data)
-        except IntegrityError:
-            raise serializers.ValidationError({
-                'detail': 'You have already rated this track.'
-            })
+
