3c1987c68060fbef7e3d6dbaeec21d61a25160db
fraserivison <fraserivison@hotmail.co.uk>
2025-01-11 14:32:20 +0000

    Add a new test test_follow_self to FollowerTests

--- followers/tests.py
+++ followers/tests.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import User
 from rest_framework.test import APITestCase
 from rest_framework import status
 from .models import Follower
-from rest_framework.exceptions import ValidationError
 
 
 class FollowerTests(APITestCase):
@@ -20,9 +19,6 @@ class FollowerTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertTrue(Follower.objects.filter(owner=self.user1, followed=self.user2).exists())
 
-        from rest_framework.exceptions import ValidationError
-
-    
     def test_duplicate_follow(self):
         """
         Test that a user cannot follow the same user twice.
@@ -31,7 +27,6 @@ class FollowerTests(APITestCase):
         self.assertEqual(response1.status_code, status.HTTP_201_CREATED)
 
         response2 = self.client.post('/followers/', {'followed': self.user2.id})
-    
         self.assertEqual(response2.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn('This user is already followed.', str(response2.data))
 
@@ -51,7 +46,6 @@ class FollowerTests(APITestCase):
 
         self.assertFalse(Follower.objects.filter(owner=self.user1, followed=self.user2).exists())
 
-
     def test_retrieve_follower_detail(self):
         """
         Test that a user can retrieve details of a specific follower relationship.
@@ -63,4 +57,15 @@ class FollowerTests(APITestCase):
 
         self.assertEqual(response.data['owner'], self.user1.username)
         self.assertEqual(response.data['followed'], self.user2.id)
+
+    def test_follow_self(self):
+        """
+        Test that a user cannot follow themselves.
+        """
+        response = self.client.post('/followers/', {'followed': self.user1.id})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('You cannot follow yourself.', str(response.data))
+
+        self.assertFalse(Follower.objects.filter(owner=self.user1, followed=self.user1).exists())
+
 
