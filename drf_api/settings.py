6f0c0d6b0a6b23892dac571f26494d8ec4da54b8
fraserivison <fraserivison@hotmail.co.uk>
2025-01-14 12:38:37 +0000

    Add token refresh to main urls

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -107,6 +107,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'rest_framework',
+    'rest_framework_simplejwt',
     'django_filters',
     'rest_framework.authtoken',
     'dj_rest_auth',
--- drf_api/urls.py
+++ drf_api/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenRefreshView
 from .views import root_route, logout_route
 
 urlpatterns = [
@@ -9,6 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/logout/', logout_route),
+    path('dj-rest-auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('auth/social/', include('allauth.socialaccount.urls')),
