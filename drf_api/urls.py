82a1a907a004d70ab4d87dc7425dd8abc3b9ef4b
fraserivison <fraserivison@hotmail.co.uk>
2025-01-16 22:48:23 +0000

    Add token urls to backend to try fix frontend PUT issue

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -2,22 +2,35 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import root_route, logout_route
 from profiles.views import CustomLoginView
 
 urlpatterns = [
+    # Root route
     path('', root_route),
+
+    # Admin and authentication endpoints
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+
+    # JWT token endpoints
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # dj-rest-auth and registration
     path('dj-rest-auth/login/', CustomLoginView.as_view(), name='custom-login'),
     path('dj-rest-auth/logout/', logout_route),
     path('dj-rest-auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+
+    # Social auth and profiles
     path('auth/social/', include('allauth.socialaccount.urls')),
     path('accounts/', include('allauth.urls')),
     path('profiles/', include('profiles.urls')),
+
+    # Other app endpoints
     path('tracks/', include('tracks.urls')),
     path('comments/', include('comments.urls')),
     path('ratings/', include('ratings.urls')),
@@ -27,5 +40,6 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 
