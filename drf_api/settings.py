f80bd24a13ac6143a004237c7d685910ff74e214
fraserivison <fraserivison@hotmail.co.uk>
2025-01-16 11:33:04 +0000

    Uncomment MEDIA path in settings

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -20,7 +20,7 @@ if os.path.exists('env.py'):
 CLOUDINARY_STORAGE = {
     'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
 }
-#MEDIA_URL = '/media/'
+MEDIA_URL = '/media/'
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
