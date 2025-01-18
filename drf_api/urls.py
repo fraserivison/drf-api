ea29fc64bb694cbd54c8f681cff6f78510060d6a
fraserivison <fraserivison@hotmail.co.uk>
2025-01-18 16:37:32 +0000

    Uncomment TokenRefreshView on main urls

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework_simplejwt.views import TokenObtainPairView #TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import root_route, logout_route
 from profiles.views import CustomLoginView
 
