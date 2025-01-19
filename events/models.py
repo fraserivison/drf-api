5fdb6de6889ffcbbd4d5a04d58f6e9e2c9e58081
fraserivison <fraserivison@hotmail.co.uk>
2025-01-19 07:23:58 +0000

    Change genre field to dropdown choices

--- events/models.py
+++ events/models.py
@@ -6,10 +6,28 @@ class Event(models.Model):
     Event model to allow users to advertise events.
     Includes details like name, date, location, genre, etc.
     """
+    GENRE_CHOICES = [
+        ('house', 'House'),
+        ('tech_house', 'Tech House'),
+        ('trance', 'Trance'),
+        ('dubstep', 'Dubstep'),
+        ('drum_and_bass', 'Drum and Bass'),
+        ('techno', 'Techno'),
+        ('electro', 'Electro'),
+        ('progressive_house', 'Progressive House'),
+        ('chillout', 'Chillout'),
+        ('other', 'Other'),
+    ]
+
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
-    genre = models.CharField(max_length=50)
+    genre = models.CharField(
+        max_length=50,
+        choices=GENRE_CHOICES,
+        blank=False,
+        null=False
+    )
     date = models.DateTimeField()
     location = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -20,4 +38,5 @@ class Event(models.Model):
 
     def __str__(self):
         return self.name
+
 
