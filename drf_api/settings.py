6ac673c32849b2a4fafebfa2ff704db242569846
Fraser <fraserivison@hotmail.co.uk>
2025-03-23 10:15:41 +0000

    Add env.py file back in

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -54,7 +54,7 @@ REST_AUTH_SERIALIZERS = {
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
