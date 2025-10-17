a5a62fae51b47717062e85b902d788a71dbbbe23
Fraser <fraserivison@hotmail.co.uk>
2025-10-17 17:12:47 +0100

    Fix: import JWT settings using django.conf.settings instead of direct import

--- drf_api/views.py
+++ drf_api/views.py
@@ -6,48 +6,46 @@ Includes routes for the root and logout functionality.
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .settings import (
-    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
-    JWT_AUTH_SECURE,
-)
+from django.conf import settings
 
 
 @api_view()
 def root_route(request):
     """
-    Handles the root API route. 
-
+    Handles the root API route.
     Returns a welcome message when accessed.
     """
     return Response({
         "message": "Welcome to my drf API!"
     })
 
+
 @api_view(['POST'])
 def logout_route(request):
     """
     Handles the logout route.
-
     Clears authentication cookies and logs out the user by setting the JWT 
     cookies to expire immediately.
     """
-    response = Response()
+    response = Response({
+        "message": "Successfully logged out."
+    })
     response.set_cookie(
-        key=JWT_AUTH_COOKIE,
+        key=settings.JWT_AUTH_COOKIE,
         value='',
         httponly=True,
         expires='Thu, 01 Jan 1970 00:00:00 GMT',
         max_age=0,
-        samesite=JWT_AUTH_SAMESITE,
-        secure=JWT_AUTH_SECURE,
+        samesite=settings.JWT_AUTH_SAMESITE,
+        secure=settings.JWT_AUTH_SECURE,
     )
     response.set_cookie(
-        key=JWT_AUTH_REFRESH_COOKIE,
+        key=settings.JWT_AUTH_REFRESH_COOKIE,
         value='',
         httponly=True,
         expires='Thu, 01 Jan 1970 00:00:00 GMT',
         max_age=0,
-        samesite=JWT_AUTH_SAMESITE,
-        secure=JWT_AUTH_SECURE,
+        samesite=settings.JWT_AUTH_SAMESITE,
+        secure=settings.JWT_AUTH_SECURE,
     )
     return response
